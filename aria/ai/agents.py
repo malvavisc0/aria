0eb1a982b24adeeaaab986c4832b840f09ab5a38
malvavisc0 <207609879+malvavisc0@users.noreply.github.com>
2025-06-19 20:27:54 +0200

    fixnig memory

--- aria/ai/agents.py
+++ aria/ai/agents.py
@@ -1,11 +1,12 @@
-import os
 from datetime import datetime
+from os import environ
 
 from agno.agent import Agent
 from agno.memory.v2.db.sqlite import SqliteMemoryDb
 from agno.memory.v2.memory import Memory
 from agno.models.ollama import Ollama
 from agno.storage.sqlite import SqliteStorage
+
 from aria.ai.configs import ARIA_AGENT_CONFIG, PROMPT_IMPROVER_AGENT_CONFIG
 from aria.ai.kits import (
     calulator_tools,
@@ -18,19 +19,26 @@ from aria.ai.kits import (
 from aria.ai.outputs import ImprovedPromptResponse
 
 OLLAMA_MODEL = Ollama(
-    id=os.getenv("OLLAMA_MODEL_ID", "cogito:8bb"),
-    host=os.getenv("OLLAMA_URL"),
+    id=environ.get("OLLAMA_MODEL_ID", "cogito:8bb"),
+    host=environ.get("OLLAMA_URL"),
     timeout=300,
     options={
-        "temperature": float(os.getenv("OLLAMA_MODEL_TEMPARATURE", 0.65)),
+        "temperature": float(environ.get("OLLAMA_MODEL_TEMPARATURE", 0.65)),
         "mirostat": 2,
         "repeat_last_n": -1,
         "top_k": 20,
         "seed": 10,
-        "num_ctx": int(os.getenv("OLLAMA_MODEL_CONTEXT_LENGTH", 20480)),
+        "num_ctx": int(environ.get("OLLAMA_MODEL_CONTEXT_LENGTH", 20480)),
     },
 )
-DEBUG_MODE = os.environ.get("DEBUG_MODE", "false").lower() == "true"
+DEBUG_MODE = environ.get("DEBUG_MODE", "false").lower() == "true"
+SESSIONS_DB_FILE = environ.get("DB_FILE", "/opt/storage/sessions.db")
+EXTRA_INFORMATION = f"""
+<additional_information>
+**Current date and time is**: {datetime.now().isoformat()}
+**Timezone is**: {environ.get("TZ","Europe/Berlin")}
+</additional_information>
+    """
 
 
 def get_ollama_core_agent(
@@ -51,18 +59,19 @@ def get_ollama_core_agent(
      Agent: An instance of the Ollama agent configured with specified parameters.
     """
 
-    db_file = os.getenv("DB_FILE", "/opt/storage/sessions.db")
-    storage = SqliteStorage(table_name="chat", db_file=db_file)
+    storage = None
     memory = None
+    num_history_runs = 0
     if enable_memory:
-        memory = Memory(db=SqliteMemoryDb(db_file=db_file))
-
-    description = f"{ARIA_AGENT_CONFIG['description']}. Current date and time is: {datetime.now().isoformat()}"
+        num_history_runs = 5
+        storage = SqliteStorage(table_name="chat", db_file=SESSIONS_DB_FILE)
+        memory_db = SqliteMemoryDb(table_name="memory", db_file=SESSIONS_DB_FILE)
+        memory = Memory(model=OLLAMA_MODEL, db=memory_db)
 
     return Agent(
         model=OLLAMA_MODEL,
         name=ARIA_AGENT_CONFIG["name"],
-        description=description,
+        description=f"{ARIA_AGENT_CONFIG['description']}\n\n{EXTRA_INFORMATION}",
         role=ARIA_AGENT_CONFIG["role"],
         instructions=ARIA_AGENT_CONFIG["instructions"],
         goal=ARIA_AGENT_CONFIG["goal"],
@@ -70,7 +79,11 @@ def get_ollama_core_agent(
         session_id=session_id,
         enable_agentic_memory=enable_memory,
         enable_user_memories=enable_memory,
-        add_datetime_to_instructions=True,
+        add_history_to_messages=enable_memory,
+        read_chat_history=enable_memory,
+        read_tool_call_history=enable_memory,
+        enable_session_summaries=enable_memory,
+        num_history_runs=num_history_runs,
         memory=memory,
         storage=storage,
         debug_mode=DEBUG_MODE,
@@ -101,7 +114,7 @@ def get_prompt_improver_agent() -> Agent:
     return Agent(
         model=OLLAMA_MODEL,
         name=PROMPT_IMPROVER_AGENT_CONFIG["name"],
-        description=PROMPT_IMPROVER_AGENT_CONFIG["description"],
+        description=f"{ARIA_AGENT_CONFIG['description']}\n\n{EXTRA_INFORMATION}",
         role=PROMPT_IMPROVER_AGENT_CONFIG["role"],
         instructions=PROMPT_IMPROVER_AGENT_CONFIG["instructions"],
         goal=PROMPT_IMPROVER_AGENT_CONFIG["goal"],
