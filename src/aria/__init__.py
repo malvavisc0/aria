1070f7a3eb23b4546dcbdd6e8102ed302dfab636
Malvavisc0 <207609879+malvavisc0@users.noreply.github.com>
2026-02-15 10:21:33 +0100

    avoiding raising exception on fresh run

--- src/aria/__init__.py
+++ src/aria/__init__.py
@@ -1,9 +1,16 @@
-from aria.cli.main import app
+from pathlib import Path
+from shutil import copyfile
 
 
 def main():
+    env_file = Path().cwd() / ".env"
+    if not env_file.exists():
+        copyfile(Path().cwd() / ".env.example", env_file)
+
+    from aria.cli.main import app
+
     app()
 
 
 if __name__ == "__main__":
-    app()
+    main()
